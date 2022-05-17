70bca5499f4805a020e39a56e2a4794868179cec
Pramod Rishal <pramod@openagent.com.au>
2022-05-17 20:33:05 +1000

    react 18 with storybook

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import "./Button.css";
 
 export interface ButtonProps {
@@ -7,8 +6,9 @@ export interface ButtonProps {
 }
 
 const Button = (props: ButtonProps) => {
-  return <button className="ut-px-4 ut-py-1 ut-text-sm ut-text-neutral-900 
+  return <button className="ut-green-400 ut-px-4 ut-py-1 ut-text-sm ut-text-neutral-900 
   ut-font-semibold ut-rounded-full ut-border ut-border-green-400 ">{props.label}</button>
+
 };
 
 export default Button;
