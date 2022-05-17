5f33452272f2bd4e01481818c64d2e309db7eed3
Pramod Rishal <pramod@openagent.com.au>
2022-05-17 16:49:31 +1000

    updates

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
+import "./Button.css";
+
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <button className="ut-px-4 ut-py-1 ut-text-sm ut-text-neutral-900 
+  ut-font-semibold ut-rounded-full ut-border ut-border-green-400 ">{props.label}</button>
 };
 
 export default Button;
