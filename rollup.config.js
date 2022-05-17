c0f9018a3050191b0d2aeefcd003c5224725e548
Pramod Rishal <pramod@openagent.com.au>
2022-05-17 22:02:55 +1000

    added tailwind css

--- rollup.config.js
+++ rollup.config.js
@@ -13,22 +13,38 @@ export default [
     input: "src/index.ts",
     output: [
       {
+        exports: 'named',
         file: packageJson.main,
         format: "cjs",
         sourcemap: true,
       },
       {
+        exports: 'named',
         file: packageJson.module,
         format: "esm",
         sourcemap: true,
       },
     ],
     plugins: [
-        peerDepsExternal(),
+      peerDepsExternal(),
+      postcss({
+        minimize: true,
+        modules: true,
+        use: {
+            sass: null,
+            stylus: null,
+            less: { javascriptEnabled: true }
+        }, //, modifyVars: antdVars }},,
+        extract: true,
+        config: {
+            path: './postcss.config.js',
+            ctx: null
+        }
+      }),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      postcss(),
+      
       terser(),
     ],
   },
